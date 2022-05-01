b0e279802ba874c92271046a1b4dfa67a0d6483e
Dana <87926571+DanaPar@users.noreply.github.com>
2022-05-01 13:18:29 +0300

    added logic for getting currency exchange rate

---
+++ src/Rate.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Currency;
+
+
+class Rate
+{
+    private string $currency;
+    private Xml $xml;
+    private string $date;
+    private float $rate;
+
+
+    public function __construct(string $date, string $currency)
+    {
+        $this->date = $date;
+        $this->xml = new Xml($this->date);
+        $this->currency = $currency;
+
+    }
+
+
+    public function getRate(): float
+    {
+
+        foreach ($this->xml->getXml()->Currencies->Currency as $currency){
+            if ($currency->ID == $this->currency){
+                 $this->rate = (float)$currency->Rate;
+            }
+
+        }
+        return $this->rate;
+    }
+}
