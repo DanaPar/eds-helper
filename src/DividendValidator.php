29b3f5c73a3a6a6c617c71f228ee4a63498eee02
Dana <87926571+DanaPar@users.noreply.github.com>
2022-05-08 16:30:11 +0300

    added validation for other inputs

--- src/DividendValidator.php
+++ src/DividendValidator.php
@@ -16,22 +16,13 @@ class DividendValidator
     {
         return strtoupper($data);
     }
-    //checks if date is past or today, if not then user must change  date
-
-    //checks if true -> dividend-tax=received
-    private function checkReceived(Dividend $dividend)
-    {
-        if(($dividend->getDividend() - $dividend->getTax()) != $dividend->getReceived()){
-            die("Check again. Looks like you entered incorrect dividend, tax or received amount values");
-        }
-    }
 
     private function validateTicker(Dividend $dividend): void
     {
         $this->validateFields($dividend->getTicker());
         $dividend->setTicker($this->upperCase($dividend->getTicker()));
     }
-
+//checks if date is past or today, if not then user must change  date
     private function validateDate(Dividend $dividend): void
     {
         $this->validateFields($dividend->getDate());
@@ -40,11 +31,24 @@ class DividendValidator
         }
     }
 
+    private function validateDividend(Dividend $dividend): void
+    {
+        $this->validateFields($dividend->getDividend());
+    }
+
     //tax can't be bigger than dividend
     private function validateTax(Dividend $dividend)
     {
         if($dividend->getTax() >= $dividend->getDividend()){
             die("Check again. Looks like you entered incorrect withholding tax");
+        }
+    }
+//checks if true -> dividend-tax=received
+    private function validateReceived(Dividend $dividend): void
+    {
+        $this->validateFields($dividend->getReceived());
+        if(($dividend->getDividend() - $dividend->getTax()) != $dividend->getReceived()){
+            die("Check again. Looks like you entered incorrect dividend, tax or received amount values");
         }
     }
 
@@ -58,7 +62,9 @@ class DividendValidator
     {
         $this->validateTicker($dividend);
         $this->validateDate($dividend);
+        $this->validateDividend($dividend);
         $this->validateTax($dividend);
+        $this->validateReceived($dividend);
         $this->validateCurrency($dividend);
     }
 
